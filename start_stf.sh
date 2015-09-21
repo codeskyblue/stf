3b665f765331ebe279057cb1dc56aa02d985b5cc
codeskyblue <codeskyblue@gmail.com>
2015-09-21 10:47:49 +0800

    change tmpdir

--- start_stf.sh
+++ start_stf.sh
@@ -11,6 +11,7 @@ case $(uname) in
 		;;
 esac
 
+export TMPDIR=$PWD/tmp
 
 node lib/cli.js local --public-ip $PUBLIC_IP \
     --auth-type openid --auth-options '["--identifier=https://login.netease.com/openid/"]' \
